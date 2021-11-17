2e7485d7071bd094531f8586ab4444790f66b42e
oleg <olgecher35@gmail.com>
2021-11-17 19:54:50 +0300

    done1

--- sources/task_4.cpp
+++ sources/task_4.cpp
@@ -4,23 +4,20 @@
 
 // Task 4.
 void remove_negative(std::vector<int>& input) {
-    for ( size_t i=0; i<size(input); ++i){
-      if ( input[i]<0)
-      {
-        auto iter=input.cbegin();
-        input.erase(iter+i);
-        break;
-      }
+  for (size_t i = 0; i < size(input); ++i) {
+    if (input[i] < 0) {
+      auto iter = input.cbegin();
+      input.erase(iter + i);
+      break;
     }
+  }
 }
 
 void remove_last_even(std::vector<int>& input) {
-    for ( size_t i=size(input); i>0; --i){
-      if ( input[i]%2==0)
-      {
-        auto iter=input.cbegin();
-        input.erase(iter+i);
-        break;
-      }
+  for (auto i = input.rbegin(); i != input.rend(); ++i) {
+    if ((*i) % 2 == 0) {
+      input.erase((i + 1).base());
+      break;
     }
+  }
 }
